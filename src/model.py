86fed5735bd7d9b9349490db59069a8d37bd3ce4
José Satiro <jardelsatiro57@gmail.com>
2025-08-08 22:18:11 -0300

    .

--- src/model.py
+++
@@ -1,7 +0,0 @@
-import scipy
-from sklearn.linear_model import LogisticRegression
-
-def train_model (X_train: scipy.sparse._csr.csr_matrix, y_train: list):
-    model = LogisticRegression(C=5)
-    model.fit(X_train, y_train)
-    return model
